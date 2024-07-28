2f53c7548eed83992ed905a799d9096086725703
Alex Gustafsson <alex-gustafsson@live.se>
2024-07-28 18:32:08 +0200

    Add WIP UI mockups

--- Sources/BBeeQ/BBeeQ.swift
+++ Sources/BBeeQ/BBeeQ.swift
@@ -4,70 +4,24 @@ import BBQProbeE
 import Combine
 import SwiftUI
 
-extension EnvironmentValues {
-  var probePeripheralManager: ProbePeripheralManager? {
-    get { self[ProbePeripheralManagerKey.self] }
-    set { self[ProbePeripheralManagerKey.self] = newValue }
-  }
-}
-
-private struct ProbePeripheralManagerKey: EnvironmentKey {
-  static let defaultValue: ProbePeripheralManager? = nil
-}
-
 private let logger = Logger(
   subsystem: Bundle.main.bundleIdentifier!, category: "Main")
 
-struct MenuBarFeedItem: View {
-  @Environment(\.probePeripheralManager) var probePeripheralManager: ProbePeripheralManager?
-
-  @State var probe: ProbePeripheral?
-
-  var body: some View {
-VStack {
-          ForEach(probePeripheralManager!.discovered.keys.sorted(), id: \.uuidString) { uuid in
-            Button(uuid.uuidString) {
-              Task {
-                guard let peripheral = probePeripheralManager?.discovered[uuid] else {
-                  return
-                }
-
-                do {
-                  try await probePeripheralManager!.connect(peripheral: peripheral)
-                } catch {
-                  print("error: \(error)")
-                }
-              }
-            }
-          }
-
-          ForEach(probePeripheralManager!.connections.keys.sorted(), id: \.uuidString) {uuid in
-            if let probe = probePeripheralManager?.connections[uuid] {
-              Text("Manufacturer: \(probe.manufacturerName)")
-              Text("Model: \(probe.modelNumber)")
-              Text("Serial: \(probe.serialNumber)")
-              Text("Firmware: \(probe.firmwareRevision)")
-
-              Text("Device name: \(probe.deviceName)")
-              Text("Grill temperature: \(probe.grillTemperature)")
-              Text("Probe temperature: \(probe.probeTemperature)")
-            }
-          }
-        }
-        .padding()
+class AppDelegate: NSObject, NSApplicationDelegate {
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    logger.info("Started RSSBar")
+    DispatchQueue.main.async {
+      NSApp.setActivationPolicy(.accessory)
+    }
   }
 }
 
 @main struct BBeeQ: App {
-  @State var probePeripheralManager = ProbePeripheralManager(queue: nil)
-
-  // TODO: May be run several times, don't use it to start fetching feeds
-  // TODO: Start task runner to load favicons on start. Then run on items and
-  // feeds once they are added.
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
   var body: some Scene {
     WindowGroup {
-      MenuBarFeedItem().environment(\.probePeripheralManager, probePeripheralManager)
+      AppView()
     }
   }
 }
