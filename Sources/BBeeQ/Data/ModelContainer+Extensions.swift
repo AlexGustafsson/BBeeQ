463d59a89da0e8a82fe561de767f2ac889402b66
Alex Gustafsson <alex-gustafsson@live.se>
2024-08-01 21:12:35 +0200

    Add persistence

--- Sources/BBeeQ/Data/ModelContainer+Extensions.swift
+++ Sources/BBeeQ/Data/ModelContainer+Extensions.swift
@@ -1,9 +1,30 @@
 import SwiftData
+import SwiftUI
 
 extension ModelContainer {
   static func initDefault() throws -> ModelContainer {
-    // TODO: For now - store only in-memory
-    let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+    #if os(macOS)
+    // The bundle identifier is always included on build - crash if it's not
+    let bundleID = Bundle.main.bundleIdentifier!
+    let applicationSupport = try FileManager.default.url(
+      for: .applicationSupportDirectory, in: .userDomainMask,
+      appropriateFor: nil, create: true)
+
+    let appSupportSubDirectory = applicationSupport.appending(
+      path: bundleID, directoryHint: .isDirectory)
+
+    let dataSubDirectory = appSupportSubDirectory.appending(
+      components: "data", "v1", directoryHint: .isDirectory)
+
+    try FileManager.default.createDirectory(
+      at: dataSubDirectory, withIntermediateDirectories: true,
+      attributes: nil)
+
+    let configuration = ModelConfiguration(
+      url: dataSubDirectory.appending(path: "data.sqlite"))
+    #elseif os(iOS)
+    let configuration = ModelConfiguration()
+    #endif
 
     return try ModelContainer(
       for: Probe.self,
