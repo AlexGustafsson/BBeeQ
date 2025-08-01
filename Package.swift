f30015362875ac335aec002e99eab5b2241d58e0
Alex Gustafsson <alex-gustafsson@live.se>
2025-08-01 10:55:33 +0200

    Update targets

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "BBeeQ", platforms: [.macOS(.v14), .iOS(.v17)],
+  name: "BBeeQ", platforms: [.macOS(.v15), .iOS(.v18)],
   products: [
     .executable(name: "BBeeQ", targets: ["BBeeQ"]),
     .executable(name: "BBeeQWidget", targets: ["BBeeQWidget"]),
